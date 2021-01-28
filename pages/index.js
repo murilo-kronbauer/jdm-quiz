239ab18b570b8a0361942f592e2587c692dc2129
Murilo Kronbauer <murilokronbauer@gmail.com>
2021-01-28 18:18:38 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components'
+import mainTheme from '../src/themes/mainTheme.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import QuizContainer from '../src/components/QuizContainer';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Widget from '../src/components/Widget';
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={mainTheme.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{mainTheme.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{mainTheme.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>Lorem iosum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner />
+    </QuizBackground>
+  )
 }
